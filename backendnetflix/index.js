97c579a319215524d88b67395c6e502237016f6e
geeksomil <gambhirsomil7@gmail.com>
2024-01-30 22:53:29 +0530

    commited

--- backendnetflix/index.js
+++ backendnetflix/index.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 var bodyParser = require("body-parser");
 var bcrypt = require("bcryptjs");
-const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 mongoose
   .connect("mongodb://localhost:27017/somildata", {
     useNewUrlParser: true,
@@ -28,6 +26,7 @@ App.use(bodyParser.json());
 var schema = new mongoose.Schema({
   email: {
     type: String,
+    unique: true,
     validate: {
       validator: validator.isEmail,
       message: "{VALUE} is not a valid email",
@@ -39,51 +38,30 @@ var schema = new mongoose.Schema({
   },
 });
 var model = new mongoose.model("netflix", schema);
-// console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
-// App.post("/api/v1/auth/google", async (req, res) => {
-//     try {
-//         console.log("hi");
-//         const { token } = req.body
-//         const ticket = await client.verifyIdToken({
-//             idToken: token,
-//             audience: process.env.REACT_APP_GOOGLE_CLIENT_ID
-//         });
-//         const { name, email, picture } = ticket.getPayload();
-//         console.log("login succesfully");
-
-//         res.status(201)
-
-//     }
-//     catch {
-//         console.log("did not login succesfully")
-//     }
-// })
 App.post("/signup", async (req, res) => {
   try {
-    console.log(req.body.password);
-    console.log(req.body.email);
     var passhash = await bcrypt.hash(req.body.password, 10);
     var m = new model({ email: req.body.email, password: passhash });
-    console.log(passhash);
     let result = await m.save();
     console.log(res.statusCode);
-    res
-      .status(201)
-      .json({ message: "user registered succesfully", a: "Sasaas" });
+    return res.status(201).json({ message: "user registered succesfully" });
   } catch {
     //console.log(result.message)
     console.log("error");
+    return res.status(404).json({ message: "error" });
   }
 });
-
+App.post("/duplicateentry", async (req, res) => {
+  let tuple = await model.findOne({ email: req.body.email });
+  console.log(tuple);
+  if (tuple != null) {
+    return res.status(404).send("duplicate entry");
+  }
+  return res.send("not duplicate");
+});
 App.post("/register", async (req, res) => {
-  // var c= new model(req.body);
-  // var result=await c.save();
-  // console.log(req.body);
-  // res.send(req.body);}/
   var r = await model.find({ email: req.body.email });
   console.log("register");
-  //abcdassd
   if (r.length == 0) {
     console.log("hi");
     res.status(404).json();
